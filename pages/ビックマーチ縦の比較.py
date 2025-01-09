e05e290d740a6058fb0411b4aa24dd9a4bb402ac
showShimada <sho.shimada.1131@gmail.com>
2025-01-09 12:21:14 +0900

    vartical

--- pages/ビックマーチ縦の比較.py
+++ pages/ビックマーチ縦の比較.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import content_modules
 
 def main():
-    st.title("カレンダーを選択して日付のリストを作成")
+    st.title("ビックマーチ縦の比較")
 
     # Main URL
     main_url = "https://www.slorepo.com/hole/e38393e38383e382afe3839ee383bce38381e69db1e7bf92e5bf97e9878ee5ba97code/"
@@ -15,7 +15,8 @@ def main():
     target_from = st.date_input('開始の日付を選択')
     target_to = st.date_input('終了の日付を選択')
 
-    target_dates = content_modules.get_date_to_choose(target_from,target_to)
+    date_to_choose = content_modules.get_date_to_choose(target_from,target_to)
+    target_dates = st.multiselect("choose date to aggregate",date_to_choose)
     
     if st.button('実行'):
         content_modules.create_vertical_data(main_url,target_dates)
