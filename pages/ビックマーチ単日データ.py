cfc7223f6874e256573403d3b170b9a432adcc0c
showShimada <sho.shimada.1131@gmail.com>
2025-01-20 18:38:04 +0900

    delete

---
+++ pages/ビックマーチ単日データ.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+#import modules
+import content_modules
+
+def main():
+    st.title("ビックマーチ単日データ作成")
+
+    target_date = st.text_input('日付を指定：YYYYMMDD')
+
+    # Main URL
+    main_url = "https://www.slorepo.com/hole/e38393e38383e382afe3839ee383bce38381e69db1e7bf92e5bf97e9878ee5ba97code/" + target_date + "/"
+
+    if st.button('実行'):
+        content_modules.create_one_day_data(main_url,"ビックマーチ東習志野")
+
+if __name__ == "__main__":
+    main()
---
+++ pages/ビックマーチ縦の比較.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from urllib.parse import quote, urljoin
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import content_modules
+
+def main():
+    st.title("ビックマーチ縦の比較")
+
+    # Main URL
+    main_url = "https://www.slorepo.com/hole/e38393e38383e382afe3839ee383bce38381e69db1e7bf92e5bf97e9878ee5ba97code/"
+
+    target_from = st.date_input('開始の日付を選択')
+    target_to = st.date_input('終了の日付を選択')
+
+    date_to_choose = content_modules.get_date_to_choose(target_from,target_to)
+    target_dates = st.multiselect("集計対象の日付を選択",date_to_choose)
+    
+    if st.button('実行'):
+        content_modules.create_vertical_data(main_url,target_dates)
+
+if __name__ == "__main__":
+    main()
